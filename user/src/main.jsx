82a6f150ebe082429cb71001ff8c159bd94bace4
yaroslav <reznikovaroslav419@gmail.com>
2023-09-09 21:02:24 +0300

    minor changes

--- user/src/main.jsx
+++ user/src/main.jsx
@@ -26,6 +26,14 @@ function MainPage() {
     const logOut = () => {
         const result = window.confirm("Are you sure you want to log out?");
         if (result) {
+            const cookies = document.cookie.split(";");
+
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i];
+                const eqPos = cookie.indexOf("=");
+                const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            }
             setShowLogIn(true);
         } else {
             console.log("User chose not to logout.");
@@ -54,43 +62,50 @@ function MainPage() {
             console.error('Error fetching data:', error);
         });  
     };
-
     const getCookie = (name) => {
         var value = "; " + document.cookie;
         var parts = value.split("; " + name + "=");
         if (parts.length == 2) return decodeURIComponent(parts.pop().split(";").shift());
     };
-
-    if (showLogIn) {
-        return <LogIn />;
-    }
-
     return (
-        <div className="MainWrapper">
-            <div className="searchBar">
-                <input 
-                    type="text" 
-                    id="search-bar" 
-                    placeholder="search bar"
-                    value={searchValue}
-                    onChange={e => setSearchValue(e.target.value)}
-                    onKeyDown={handleKeyDown} 
-                />
-            </div>
-            <div className="userDatacon">
-                <div className="user-data">
-                    <label>{username}</label>
+        <>
+            {showLogIn ? (
+                <LogIn />
+            ) : (
+                <div className="globalWrapper">
+                <div className="MainWrapper">
+                    <div className="add-good-btn">
+                        <button>
+                            add good
+                        </button>
+                    </div>
+                    <div className="searchBar">
+                        <input 
+                            type="text" 
+                            id="search-bar" 
+                            placeholder="search bar"
+                            value={searchValue}
+                            onChange={e => setSearchValue(e.target.value)}
+                            onKeyDown={handleKeyDown} 
+                        />
+                    </div>
+                    <div className="userPersonalData">
+                        <div className="user-data">
+                            <label>{username}</label>
+                        </div>
+                        <div className="userbtn">
+                            <button className="btn" onClick={logOut}>Log out</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="userbtn">
-                    <button className="btn" onClick={logOut}>Log out</button>
+                <div className="user-seen-part">
+                    <div className="goods-field">
+                        <label>There should be data</label>
+                    </div>
                 </div>
             </div>
-            <div className="user-seen-part">
-                <div className="goods-field">
-                    <label>There should be data</label>
-                </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 }
 
